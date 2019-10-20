7581da0238b8219ded24d351a8dd9cb0c0575eb2
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-10-20 05:24:19 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -4,7 +4,6 @@ class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.2]
      add_column :students, :grade, :integer
      add_column :students, :birthdate, :string
    
-    end
   end
 
 end
